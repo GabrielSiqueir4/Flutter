203c3a75ed02820ebcabb2ff98dabd9efbcf5c82
Gabriel Da Silva Siqueira <99335926+GabrielSiqueir4@users.noreply.github.com>
2022-05-11 20:56:45 -0300

    TESTE

--- AulaCod3er/lib/components/chart.dart
+++ AulaCod3er/lib/components/chart.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:intl/intl_browser.dart';
 import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
