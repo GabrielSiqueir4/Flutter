9af19fe99a1951b5080d2a265ca1db763145029b
Gabriel Da Silva Siqueira <99335926+GabrielSiqueir4@users.noreply.github.com>
2022-05-10 21:47:25 -0300

    aula

---
+++ ProgramaAula/CONTAS/src/main/java/BR/FAG/EDU/CONTAS/ContasApplication.java
@@ -0,0 +1,13 @@
+package BR.FAG.EDU.CONTAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContasApplication.class, args);
+	}
+
+}
---
+++ ProgramaAula/CONTAS/src/test/java/BR/FAG/EDU/CONTAS/ContasApplicationTests.java
@@ -0,0 +1,13 @@
+package BR.FAG.EDU.CONTAS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
