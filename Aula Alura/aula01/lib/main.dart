f68bac67d389cd51b9572b5b0ec596f97a195351
Gabriel Da Silva Siqueira <99335926+GabrielSiqueir4@users.noreply.github.com>
2022-05-27 01:24:24 -0300

    edição

--- Aula Alura/aula01/lib/main.dart
+++ Aula Alura/aula01/lib/main.dart
@@ -1,22 +1,75 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
 
-import 'ListaTransferencias.dart';
+void main() => runApp(BytebankApp());
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          body: ListaTransferencias(),
-          appBar: AppBar(
-            title: Text('Transferencias'),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            child: Icon(Icons.add_outlined),
-          ),
-        ),
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
       ),
     );
+  }
+}
 
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Criando Transferencia')),
+      body: Text('teste'),
+    );
+  }
+}
 
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferencias'),
+      ),
+      body: Column(
+        // ignore: prefer_const_literals_to_create_immutables
+        children: [
+          ItemTransferencia(Transferencia(100, 200)),
+          ItemTransferencia(Transferencia(100, 200)),
+          ItemTransferencia(Transferencia(100, 200)),
+          ItemTransferencia(Transferencia(100, 200)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add_outlined),
+      ),
+    );
+  }
+}
+
+// ignore: use_key_in_widget_constructors
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on_rounded),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.valor.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
