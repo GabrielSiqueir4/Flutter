2a1fd4b42bf7f19fef8f8d51c0ce36476d71c349
Gabriel Da Silva Siqueira <99335926+GabrielSiqueir4@users.noreply.github.com>
2022-06-08 09:42:31 -0300

    aula

---
+++ Aula Alura/aula01/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\gabriel.siqueira\\OneDrive - INSIDE SISTEMAS LTDA\\Documentos\\GitHub\\Flutter\\Aula Alura\\aula01" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Users\\gabriel.siqueira\\OneDrive - INSIDE SISTEMAS LTDA\\Documentos\\GitHub\\Flutter\\Aula Alura\\aula01"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\gabriel.siqueira\\OneDrive - INSIDE SISTEMAS LTDA\\Documentos\\GitHub\\Flutter\\Aula Alura\\aula01\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\gabriel.siqueira\\OneDrive - INSIDE SISTEMAS LTDA\\Documentos\\GitHub\\Flutter\\Aula Alura\\aula01"
+  "FLUTTER_TARGET=C:\\Users\\gabriel.siqueira\\OneDrive - INSIDE SISTEMAS LTDA\\Documentos\\GitHub\\Flutter\\Aula Alura\\aula01\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\gabriel.siqueira\\OneDrive - INSIDE SISTEMAS LTDA\\Documentos\\GitHub\\Flutter\\Aula Alura\\aula01\\.dart_tool\\package_config.json"
+)
